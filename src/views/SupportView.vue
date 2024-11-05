9d24bf3be687973bf1ad71783d8b2678aa086a6d
JR Duboc <jr.duboc@gmail.com>
2024-11-05 18:41:10 +0000

    Added link to Crowdfunder and updated bot script (still need centre code0

--- src/views/SupportView.vue
+++ src/views/SupportView.vue
@@ -4,6 +4,22 @@
     <p class="text-3xl">
       Our money mentors are here to help and support anyone worried about debt and provide access to FREE advice, whatever their circumstances, background or faith.
     </p>
+    <h2>Make a donation</h2>
+    <p class="text-2xl">
+      You can make a donatio to help cover our running costs.<br/>
+      These include:
+      <ul class="text-2xl list-disc list-inside my-3">
+        <li>Our office expenses: IT and communication, phone line, etc.</li>
+        <li>Our contributions to Community Money Advice. 
+            CMA's annual fee is £2550 a year, and enables them to pay the debt advisors we work with.
+        </li>
+        <li>Our fundraising for a part-time centre manager.</li>
+      </ul>
+
+      <div class="crowdfunder-widget" data-project-title="CMA CONNECT FAREHAM" data-project-id="cma-connect-fareham" data-template-id="4185379753100011" data-theme="default" data-style-height="48px" data-style-width="240px">
+        <a href="https://www.crowdfunder.co.uk/p/cma-connect-fareham" rel="noopener" target="_blank">CMA CONNECT FAREHAM</a>
+      </div>
+    </p>
     <h2>Got a head for finance and a ♥ for people?</h2>
     <p class="text-2xl">
       <b>Come and volunteer at CMA Connect Fareham</b><br/>
