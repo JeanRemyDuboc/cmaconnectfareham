2ea121f249f11d0015f30a14b5ecd1598b8841a4
JR Duboc <jr.duboc@gmail.com>
2025-04-05 15:56:45 +0100

    Added Acts 435

--- src/views/WhoView.vue
+++ src/views/WhoView.vue
@@ -7,6 +7,19 @@
       times, there is hope for the future. We want to share that hope with
       anyone struggling with debt in the Fareham and Gosport area.
     </p>
+    <h2>Acts 435 Partner - Emergency Fund for Urgent Needs</h2>
+    <p class="text-3xl">
+      We are also <a href="https://www.acts435.org.uk/partners/1512/">partners with Acts 435</a>, a crowdfunding charity that provides financial help (up to £200) for urgent needs.<br/>
+      This enables us to support our client as well as other people in our community who need our support.
+      <br/>
+      <a class="flex justify-center" href="https://www.acts435.org.uk/partners/1512/">
+        <img 
+          alt="Support us"
+          src="@/assets/acts_435.svg"  
+          class="w-44 m-10"
+        /> 
+      </a>    
+    </p>
     <div class="py-14 border-b flex justify-center">
       <RouterLink class="text-3xl block no-underline" to="/support-us">
         Support us 
